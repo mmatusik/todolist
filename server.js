03fb8cb2851db74fc3f605c94662137a6e564939
Michał <mmatusikpl@gmail.com>
2018-01-30 23:16:04 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'), //created model loading here
+  bodyParser = require('body-parser'),
+  cors = require('cors');
+  
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://x2008x:wasko123@ds115198.mlab.com:15198/matusik');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+var cons = require('consolidate');
+
+var corsOptions = {
+  origin: 'http://localhost',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+ 
+app.get('/tasks/:id', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for only example.com.'})
+})
+
+// view engine setup
+app.use(express.static(__dirname + '/views'));
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
